c92375cea23602f2ca3a13a22b5f813b94a6d16f
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-01-22 15:58:13 -0300

    style and project update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        Portfólio
       </a>
       <div className={styles.menu}>
         <img
