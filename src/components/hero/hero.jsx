141b7beaa5a9d751f61bf7f677e78776045dd6ee
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-05-14 09:18:41 -0300

    style: add contact's icons on Hero and responsive att

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -12,12 +12,34 @@ export const Hero = () => {
         <h3 className={styles.subtitle}>Desenvolvedor de Software e Psicólogo.</h3>
         Curso Análise e desenvolvimento de sistemas e estou engajado na trilha para ser um desenvolvedor Full-Stack, crescendo e evoluindo a cada passo. 
         </p>
-        <a
-          href="mailto:filipesantos.psic@gmail.com"
-          className={styles.contactBtn}
-        >
-          Entre em contato!
-        </a>
+
+        <div className={styles.contactContainer}>
+        <ul className={styles.links}>
+        <li className={styles.link}>
+          <a href="mailto:filipesantos.psic@gmail.com">
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          </a>
+        </li>
+
+        <li className={styles.link}>
+          <a
+            href="https://www.linkedin.com/in/dos-santos-filipe"
+            target="_blank"
+          >
+            <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="Linkedin icon"
+          />
+          </a>
+        </li>
+
+        <li className={styles.link}>
+          <a href="https://github.com/Dos-Santos-Filipe" target="_blank">
+            <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          </a>
+        </li>
+      </ul>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/CN-tower-profile-photo.jpeg")}
